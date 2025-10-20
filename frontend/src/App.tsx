12b9cc287f12875c259fe57743aeae9f46ad6918
Sknery <letsplayfrommax14@gmail.com>
2025-10-20 11:05:29 +0000

    Tailwind css setting

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="bg-slate-900 text-white h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Установка завершена!</h1>
+    </div>
+  )
+}
+export default App
